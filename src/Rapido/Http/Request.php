5f3fc3f603841e420b070ac3996f0ab4509493be
Vincent Letourneau <vincent@nanoninja.com>
2022-05-31 15:32:21 +0200

    Récupération de la méthode et du body

--- src/Rapido/Http/Request.php
+++ src/Rapido/Http/Request.php
@@ -3,9 +3,96 @@
 namespace Rapido\Http;
 
 class Request {
-    private array $params = [];
-    private array $queryParams = [];
+    private array $params = [];       // /cart/product/{20}
+    private array $queryParams = [];  // ?name=toto&age=30
+    private array $serverParams = []; // $_SERVER
     private string $method = 'GET';
-    private string $body = '';
+    private ?string $body;
     private Uri $uri;
+
+    public function __construct(Uri $uri)
+    {
+        $this->setUri($uri);
+    }
+
+    public function getUri(): Uri
+    {
+        return $this->uri;
+    }
+
+    public function setUri(Uri $uri): self
+    {
+        $queryParams = [];
+        parse_str($uri->getQuery(), $queryParams);
+        $this->setQueryParams($queryParams);
+
+        $this->uri = $uri;
+
+        return $this;
+    }
+ 
+    public function getBody(): string
+    {
+        if (is_null($this->body)) {
+            $this->body = stream_get_contents('php://input');
+        }
+
+        return $this->body;
+    }
+
+    public function setBody(string $body): self
+    {
+        $this->body = $body;
+
+        return $this;
+    }
+
+    public function getMethod(): string
+    {
+        return $this->method;
+    }
+
+    public function setMethod(string $method): self
+    {
+        $this->method = $method;
+
+        return $this;
+    }
+
+    public function getQueryParams(): array
+    {
+        return $this->queryParams;
+    }
+
+    public function setQueryParams(array $queryParams): self
+    {
+        $this->queryParams = $queryParams;
+
+        return $this;
+    }
+
+    public function getServerParams(): array
+    {
+        return $this->serverParams;
+    }
+
+    public function setServerParams(array $serverParams): self
+    {
+        $this->setMethod($serverParams['REQUEST_METHOD'] ?? 'GET');
+        $this->serverParams = $serverParams;
+
+        return $this;
+    }
+
+    public function getParams(): array
+    {
+        return $this->params;
+    }
+
+    public function setParams(array $params): self
+    {
+        $this->params = $params;
+
+        return $this;
+    }
 }
--- web/index.php
+++ web/index.php
@@ -3,28 +3,25 @@
 ini_set('display_errors', 'On');
 
 use Rapido\Http\Header;
+use Rapido\Http\Request;
 use Rapido\Http\Response;
 use Rapido\Http\Uri;
 
 require '../vendor/autoload.php';
 
 $uri = new Uri($_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+$request = new Request($uri);
+$request->setServerParams($_SERVER);
+
+var_dump($request->getQueryParams());
+
 $header = new Header();
 $response = new Response();
+
 $response->setHeader($header);
 
 $header->set('Content-Type', 'text/html; charset=utf-8')
 ->set('X-Powered-By', 'Rapido Framework');
 
 $response->setStatus(404)
-->send('<h1>Hello World</h1>');
-
-// var_dump($_SERVER);
-
-echo $uri->getHost(), '<br>';
-echo $uri->getPath(), '<br>';
-echo $uri->getPort(), '<br>';
-echo $uri->getUser(), '<br>';
-echo $uri->getPass(), '<br>';
-echo $uri->getFragment(), '<br>';
-echo $uri->getQuery(), '<br>';
+    ->send('<h1>Hello World</h1>');
