7c5756aab369f874b50364dbbcb1c7e837fab0cb
Vincent Letourneau <vincent@nanoninja.com>
2022-06-01 15:36:46 +0200

    Ajout de PDO service

--- web/config/services.php
+++ web/config/services.php
@@ -11,8 +11,8 @@ function initServices(Container $container): Container
 
     // Database
     $container->register('db:dsn', 'mysql:host=localhost;dbname=blogger;charset=utf8');
-    $container->singleton('db', function ($c) {
-
+    $container->singleton('db', funcwtion ($c) {
+        return new PDO($c->get('dsn'), 'root', 'root');
     });
 
     // HTTP
